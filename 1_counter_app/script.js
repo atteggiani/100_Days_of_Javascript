1f53f31df4d2fe778ad11429255567bbf3e7eae3
atteggiani <davide.marchegiani@gmail.com>
2022-03-01 01:30:14 +1100

    Completed project 1: Counter App

---
+++ 1_counter_app/script.js
@@ -0,0 +1,28 @@
+const count = document.querySelector(".count");
+const buttons = document.querySelector(".buttons");
+
+buttons.addEventListener("click", (e) => {
+    if (e.target.classList.contains("add")) {
+        count.innerHTML++;
+        setColor()
+    }
+    if (e.target.classList.contains("subtract")) {
+        count.innerHTML--;
+        setColor()
+    }
+    if (e.target.classList.contains("reset")) {
+        count.innerHTML=0;
+        setColor()
+    }
+});
+
+function setColor() {
+    if (count.innerHTML > 0) {
+        count.style.color = "rgba(26, 145, 15, 0.934)";
+    } else if (count.innerHTML < 0) {
+        count.style.color = "rgba(254, 99, 2, 0.934)";
+    } else {
+        count.style.color = "#fff";
+    }
+    
+}
